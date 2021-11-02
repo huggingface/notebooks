1dea70f388510e2c2eefb5c4892830d9f4b3308e
Philipp Schmid <schmidphilipp1995@gmail.com>
2021-11-02 14:16:27 +0100

    added new to_tf_dataset method

--- sagemaker/02_getting_started_tensorflow/scripts/train.py
+++ sagemaker/02_getting_started_tensorflow/scripts/train.py
@@ -5,7 +5,7 @@ import sys
 
 import tensorflow as tf
 from datasets import load_dataset
-from transformers import AutoTokenizer, TFAutoModelForSequenceClassification
+from transformers import AutoTokenizer, TFAutoModelForSequenceClassification, create_optimizer
 
 
 if __name__ == "__main__":
@@ -16,10 +16,8 @@ if __name__ == "__main__":
     parser.add_argument("--epochs", type=int, default=3)
     parser.add_argument("--train_batch_size", type=int, default=16)
     parser.add_argument("--eval_batch_size", type=int, default=8)
-    parser.add_argument("--model_name", type=str)
-    parser.add_argument("--learning_rate", type=str, default=5e-5)
-    parser.add_argument("--do_train", type=bool, default=True)
-    parser.add_argument("--do_eval", type=bool, default=True)
+    parser.add_argument("--model_id", type=str)
+    parser.add_argument("--learning_rate", type=str, default=3e-5)
 
     # Data, model, and output directories
     parser.add_argument("--output_data_dir", type=str, default=os.environ["SM_OUTPUT_DATA_DIR"])
@@ -37,78 +35,78 @@ if __name__ == "__main__":
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     )
 
-    # Load model and tokenizer
-    model = TFAutoModelForSequenceClassification.from_pretrained(args.model_name)
-    tokenizer = AutoTokenizer.from_pretrained(args.model_name)
+    # Load tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(args.model_id)
 
-    # Load dataset
-    train_dataset, test_dataset = load_dataset("imdb", split=["train", "test"])
-    train_dataset = train_dataset.shuffle()
-    test_dataset = test_dataset.shuffle()
-    
+    # Load DatasetDict
+    dataset = load_dataset("imdb")
+
     # Preprocess train dataset
-    train_dataset = train_dataset.map(
-        lambda e: tokenizer(e["text"], truncation=True, padding="max_length"), batched=True
-    )
-    train_dataset.set_format(type="tensorflow", columns=["input_ids", "attention_mask", "label"])
+    def preprocess_function(examples):
+        return tokenizer(examples["text"], truncation=True)
 
-    train_features = {
-        x: train_dataset[x].to_tensor(default_value=0, shape=[None, tokenizer.model_max_length])
-        for x in ["input_ids", "attention_mask"]
-    }
-    tf_train_dataset = tf.data.Dataset.from_tensor_slices((train_features, train_dataset["label"])).batch(
-        args.train_batch_size
-    )
+    encoded_dataset = dataset.map(preprocess_function, batched=True)
 
-    # Preprocess test dataset
-    test_dataset = test_dataset.map(
-        lambda e: tokenizer(e["text"], truncation=True, padding="max_length"), batched=True
-    )
-    test_dataset.set_format(type="tensorflow", columns=["input_ids", "attention_mask", "label"])
+    # define tokenizer_columns
+    # tokenizer_columns is the list of keys from the dataset that get passed to the TensorFlow model
+    tokenizer_columns = ["attention_mask", "input_ids"]
 
-    test_features = {
-        x: test_dataset[x].to_tensor(default_value=0, shape=[None, tokenizer.model_max_length])
-        for x in ["input_ids", "attention_mask"]
-    }
-    tf_test_dataset = tf.data.Dataset.from_tensor_slices((test_features, test_dataset["label"])).batch(
-        args.eval_batch_size
+    # convert to TF datasets
+    tf_train_dataset = encoded_dataset["train"].to_tf_dataset(
+        columns=tokenizer_columns,
+        label_cols=["label"],
+        shuffle=True,
+        batch_size=args.train_batch_size,
+        collate_fn=tokenizer.pad,
+    )
+    tf_validation_dataset = encoded_dataset["text"].to_tf_dataset(
+        columns=tokenizer_columns,
+        label_cols=["label"],
+        shuffle=False,
+        batch_size=args.eval_batch_size,
+        collate_fn=tokenizer.pad,
     )
 
-    # fine optimizer and loss
-    optimizer = tf.keras.optimizers.Adam(learning_rate=args.learning_rate)
+    # Prepare model labels - useful in inference API
+    labels = dataset["train"].features["labels"].names
+    num_labels = len(labels)
+    label2id, id2label = dict(), dict()
+    for i, label in enumerate(labels):
+        label2id[label] = str(i)
+        id2label[str(i)] = label
+
+    # download model from model hub
+    model = TFAutoModelForSequenceClassification.from_pretrained(
+        args.model_id, num_labels=num_labels, label2id=label2id, id2label=id2label
+    )
+
+    # create Adam optimizer with learning rate scheduling
+    batches_per_epoch = len(encoded_dataset["train"]) // args.train_batch_size
+    total_train_steps = int(batches_per_epoch * args.epochs)
+
+    optimizer, _ = create_optimizer(init_lr=args.learning_rate, num_warmup_steps=0, num_train_steps=total_train_steps)
     loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+    # define metric and compile model
     metrics = [tf.keras.metrics.SparseCategoricalAccuracy()]
     model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
 
     # Training
-    if args.do_train:
+    train_results = model.fit(
+        tf_train_dataset,
+        epochs=args.epochs,
+        validation_data=tf_validation_dataset,
+    )
+    logger.info("*** Train ***")
 
-        train_results = model.fit(tf_train_dataset, epochs=args.epochs, batch_size=args.train_batch_size)
-        logger.info("*** Train ***")
+    output_eval_file = os.path.join(args.output_data_dir, "train_results.txt")
 
-        output_eval_file = os.path.join(args.output_data_dir, "train_results.txt")
-
-        with open(output_eval_file, "w") as writer:
-            logger.info("***** Train results *****")
-            logger.info(train_results)
-            for key, value in train_results.history.items():
-                logger.info("  %s = %s", key, value)
-                writer.write("%s = %s\n" % (key, value))
-
-    # Evaluation
-    if args.do_eval:
-
-        result = model.evaluate(tf_test_dataset, batch_size=args.eval_batch_size, return_dict=True)
-        logger.info("*** Evaluate ***")
-
-        output_eval_file = os.path.join(args.output_data_dir, "eval_results.txt")
-
-        with open(output_eval_file, "w") as writer:
-            logger.info("***** Eval results *****")
-            logger.info(result)
-            for key, value in result.items():
-                logger.info("  %s = %s", key, value)
-                writer.write("%s = %s\n" % (key, value))
+    with open(output_eval_file, "w") as writer:
+        logger.info("***** Train results *****")
+        logger.info(train_results)
+        for key, value in train_results.history.items():
+            logger.info("  %s = %s", key, value)
+            writer.write("%s = %s\n" % (key, value))
 
     # Save result
     model.save_pretrained(args.model_dir)
